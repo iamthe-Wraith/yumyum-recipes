5d20fca3b83e7c48c94b27906bd376e818e7a562
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-03-23 07:27:16 -0400

    updates nav so does not require js to toggle open/close
    the nav toggle is now purely html and css so is more accessible and does
    not require js. the only js that remians is a small listener that
    adds/removes a class from the nav overlay to prevent it from
    transitioning while the screen is being resized.
    
    this update also adds a desktop nav instead of reusing the mobile nav.

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,34 +1,43 @@
 <script lang="ts">
-	import { log } from '$lib/services/log';
-	import { authenticated } from '$lib/stores/authenticated';
-  import { fly } from 'svelte/transition';
-	import ToggleMenuButton from "./ToggleMenuButton.svelte";
+	import { authenticated } from "$lib/stores/authenticated";
+	import { onDestroy, onMount } from "svelte";
 
-  let isOpen = false;
+  let navOverlay: HTMLDivElement;
+  let noTransitionTimer: number;
 
-  const toggleNav = () => {
-    log('toggling nav...');
+  const onResize = () => {
+    console.log('resizing...');
 
-    isOpen = !isOpen;
+    clearTimeout(noTransitionTimer);
+
+    navOverlay.classList.add('no-transition');
+
+    noTransitionTimer = window.setTimeout(() => {
+      navOverlay.classList.remove('no-transition');
+    }, 500);
   }
+
+  onMount(() => {
+    // could not use svelte:window here because need to be
+    // able to remove the event listener when this component
+    // is destroyed.
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    }
+  });
 </script>
 
-<ToggleMenuButton on:click={toggleNav} {isOpen} />
+<div class="nav-toggle-container">
+  <input type="checkbox" id="nav-checkbox" class="nav-checkbox" />
 
-{#if isOpen}
-  <button
-    tabindex="-1"
-    class='nav-overlay'
-    transition:fly={{ x: 50 }}
-    on:click={toggleNav}
-    aria-label="Close Navigation"
-    aria-expanded={isOpen}
-  >
-    <div
-      class="nav-container"
-      on:click|stopPropagation={() => {}}
-      on:keydown|stopPropagation={() => {}}
-    >
+  <label for="nav-checkbox" class="nav-toggle" aria-label="navigation toggle">
+    <span class="nav-toggle-line" />
+  </label>
+
+  <div class="nav-overlay" bind:this={navOverlay}>
+    <div class="nav-container">
       <nav>
         {#if $authenticated}
           <a href="/dashboard">Dashboard</a>
@@ -39,11 +48,79 @@
         {/if}
       </nav>
     </div>
-  </button>
-{/if}
+  </div>
+</div>
 
 <style lang="scss">
-  .nav-overlay {
+  .nav-toggle-container {
+    position: relative;
+    width: 2rem;
+    height: 1.25rem;
+  }
+
+  .nav-checkbox {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    opacity: 1;
+    appearance: none;
+
+    &:checked ~ .nav-toggle {
+      &:before {
+        top: 50%;
+        transform: rotate(45deg);
+      }
+
+      &:after {
+        top: 50%;
+        transform: rotate(-45deg);
+      }
+
+      .nav-toggle-line {
+        opacity: 0;
+      }
+    }
+  }
+
+  .nav-toggle {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    .nav-toggle-line,
+    &:before,
+    &:after {
+      position: relative;
+      width: 100%;
+      height: .1rem;
+      background: var(--neutral-500);
+      transition: .25s ease-in-out;
+      opacity: 1;
+    }
+
+    &:before,
+    &:after {
+      content: ' ';
+      position: absolute;
+    }
+
+    &:before {
+      top: 0;
+    }
+
+    &:after {
+      top: calc(100% - 0.1rem);
+    }
+  }
+
+  .nav-checkbox ~ .nav-overlay {
     position: fixed;
     top: 85px;
     bottom: 0;
@@ -54,6 +131,20 @@
     background: rgba(0, 0, 0, 0.5);
     border: none;
     outline: none;
+    opacity: 0;
+    visibility: hidden;
+  }
+
+  .nav-checkbox:checked ~ .nav-overlay,
+  .nav-checkbox:checked ~ .nav-overlay {
+    visibility: visible;
+    opacity: 1;
+  }
+
+  .nav-checkbox ~ .nav-overlay:not(.no-transition),
+  .nav-checkbox:checked ~ .nav-overlay:not(.no-transition),
+  .nav-checkbox:checked ~ .nav-overlay:not(.no-transition) {
+    transition: .25s ease-in-out;
   }
 
   .nav-container {
@@ -76,6 +167,44 @@
       &:focus {
         color: var(--primary-500);
         text-decoration: underline;
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    .nav-toggle-container {
+      width: 100%;
+      height: auto;
+    }
+
+    .nav-checkbox,
+    .nav-toggle {
+      display: none;
+    }
+
+    .nav-checkbox ~ .nav-overlay {
+      position: static;
+      display: flex;
+      justify-content: flex-end;
+      width: 100%;
+      outline: none;
+      opacity: 1;
+      visibility: visible;
+
+      .nav-container {
+        border-left: none;
+        background: none;
+
+        nav {
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          height: 100%;
+
+          & > * {
+            margin-left: 1.5rem;
+          }
+        }
       }
     }
   }
