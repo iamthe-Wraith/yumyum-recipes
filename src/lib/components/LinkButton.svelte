04670935067e45b27f0a672ba529a61935a7d191
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-03-24 07:39:36 -0400

    adds sign up buttons to homepage

---
+++ src/lib/components/LinkButton.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import type { ColorKind } from '$types';
+
+  export let href: string;
+  export let type: ColorKind = 'primary';
+</script>
+
+<a
+  {href}
+  class={type}
+>
+  <slot />
+</a>
+
+<style lang="scss">
+  a {
+    display: inline-block;
+    width: var(--link-button-width, 'auto');
+    height: var(--link-button-height, 'auto');
+    margin: var(--link-button-margin, 0.5rem);
+    padding: 0.5rem 1rem;
+    font-size: var(--link-button-font-size, 1rem);
+    border-radius: 0.25rem;
+    text-decoration: none;
+    text-align: center;
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  }
+
+  a:hover {
+    background-color: #eee;
+  }
+
+  a.primary {
+    background-color: var(--primary-500);
+    color: var(--neutral-900);
+
+    &:hover,
+    &:focus {
+      background-color: var(--primary-300);
+      color: var(--neutral-700);
+    }
+  }
+
+  a.secondary {
+    background-color: var(--secondary-500);
+    color: var(--neutral-900);
+
+    &:hover {
+      background-color: var(--secondary-300);
+      color: var(--neutral-700);
+    }
+
+    &:focus {
+      outline-color: var(--secondary-500);
+    }
+  }
+
+  a.tertiary {
+    background-color: var(--tertiary-500);
+    color: var(--neutral-900);
+
+    &:hover {
+      background-color: var(--tertiary-300);
+      color: var(--neutral-700);
+    }
+
+    &:focus {
+      outline-color: var(--tertiary-500);
+    }
+  }
+
+  a.neutral {
+    background-color: var(--neutral-500);
+    color: var(--neutral-900);
+
+    &:hover {
+      background-color: var(--neutral-300);
+      color: var(--neutral-700);
+    }
+
+    &:focus {
+      outline-color: var(--neutral-500);
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,41 @@
 <script lang="ts">
+	import LinkButton from "$lib/components/LinkButton.svelte";
+	import { authenticated } from "$lib/stores/authenticated";
 	import Hero from "./Hero.svelte";
 	import Steps from "./Steps.svelte";
 </script>
 
 <Hero />
+
+{#if !$authenticated}
+	<div class="signup-container top">
+		<LinkButton href="/dashboard">
+			Sign Up!
+		</LinkButton>
+	</div>
+{/if}
+
 <Steps />
+
+{#if !$authenticated}
+	<div class="signup-container bottom">
+		<LinkButton href="/dashboard">
+			Sign Up!
+		</LinkButton>
+	</div>
+{/if}
+
+<style lang="scss">
+	.signup-container {
+		display: flex;
+		justify-content: center;
+		margin-top: 2rem;
+		--link-button-width: 10rem;
+
+		@media (min-width: 768px) {
+			&.bottom {
+				display: none;
+			}
+		}
+	}
+</style>
