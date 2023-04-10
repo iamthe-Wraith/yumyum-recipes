da5704e3fe59fcd5ae36b365dcc20aaaca0e8ad3
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-04-10 16:08:01 -0400

    updates TextArea border with gradient colors

--- src/lib/components/TextArea.svelte
+++ src/lib/components/TextArea.svelte
@@ -8,9 +8,10 @@
   export let name = id;
   export let value = '';
   export let required = false;
+  export let appearance: 'primary-tertiary' | 'secondary-primary' | 'tertiary-secondary' = 'primary-tertiary';
 </script>
 
-<div class='input-field-container'>
+<div class='input-field-container {appearance}'>
   {#if label}
     <label for={id}>{label}</label>
   {/if}
@@ -19,12 +20,14 @@
     <p>{description}</p>
   {/if}
 
-  <textarea
-    {id}
-    {name}
-    {value}
-    {required}
-  />
+  <span>
+    <textarea
+      {id}
+      {name}
+      {value}
+      {required}
+    />
+  </span>
 
   {#if error}
     <ErrorText>{error}</ErrorText>
@@ -37,6 +40,30 @@
     flex-direction: column;
     width: var(--textarea-width, 100%);
     margin-bottom: var(--text-area-margin-bottom, 1rem);
+
+    &.primary-tertiary > span {
+      background: linear-gradient(135deg, var(--primary-500) 0%, var(--tertiary-300) 100%);
+
+      textarea:focus {
+        border-right: 4px solid var(--tertiary-300);
+      }
+    }
+    
+    &.secondary-primary > span {
+      background: linear-gradient(135deg, var(--secondary-500) 0%, var(--primary-400) 100%);
+
+      textarea:focus {
+        border-right: 4px solid var(--primary-400);
+      }
+    }
+    
+    &.tertiary-secondary > span {
+      background: linear-gradient(135deg, var(--tertiary-500) 0%, var(--secondary-400) 100%);
+
+      textarea:focus {
+        border-right: 4px solid var(--secondary-400);
+      }
+    }
   }
 
   label {
@@ -50,24 +77,24 @@
     line-height: 1.2rem;
   }
 
+  span {
+    padding: 1px;
+    border-radius: 0.25rem;
+  }
+
   textarea {
     width: var(--textarea-width, 100%);
     height: var(--textarea-height, 5rem);
     padding: 0.5rem;
-    border: 1px solid #000;
-    border-radius: 0.25rem;
     background: var(--neutral-300);
     color: var(--neutral-900);
-    border: 1px solid var(--neutral-500);
+    border: none;
+    border-radius: 0.25rem;
     box-shadow: inset 10px 0 15px -15px var(--neutral-100);
     resize: none;
     
     &::placeholder {
       color: var(--neutral-700);
-    }
-
-    &:focus {
-      border-right: 4px solid var(--primary-500);
     }
   }
 </style>
