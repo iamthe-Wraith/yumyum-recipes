54d8885d1ec4e4eb5341a9366d586ad8c2eab285
iamthe-Wraith <allhailthewraith@protonmail.com>
2023-04-17 08:59:05 -0400

    adds meal_plans model

--- prisma/migrations/20230409172915_change_recipe_img_to_image/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `img` on the `recipes` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "recipes" DROP COLUMN "img",
-ADD COLUMN     "image" TEXT;
