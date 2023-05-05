4ab3eaec643e7fe6a276d928223fab1566171dc6
Jake Lundberg <allhailthewraith@protonmail.com>
2023-05-05 07:06:12 -0400

    Issue/54 auth hook error handling update (#104)

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { enhance } from "$app/forms";
 	import Button from "$lib/components/Button.svelte";
+	import ErrorText from "$lib/components/ErrorText.svelte";
   import InputField from "$lib/components/InputField.svelte";
 	import Page from "$lib/components/Page.svelte";
   import { isErrorStatus } from "$lib/helpers/response";
@@ -12,27 +13,42 @@
 <Page>
   <div class="signin-container">
     <h1>Sign in</h1>
-    <form method="POST" use:enhance>
-      <InputField
-        label="Email"
-        type="email"
-        id="email"
-        name="email"
-        value={form?.data?.email ?? ''}
-        error={isErrorStatus(form?.status) && form?.field === 'email' ? form.message : ''}
-        appearance="secondary-primary"
-      />
+    <form
+      method="POST"
+      use:enhance
+    >
+      <div class="input-field-container">
+        <InputField
+          label="Email"
+          type="email"
+          id="email"
+          name="email"
+          value={form?.data?.email ?? ''}
+          error={isErrorStatus(form?.status) && form?.field === 'email' ? form.message : ''}
+          appearance="secondary-primary"
+        />
+      </div>
   
-      <InputField
-        label="Password"
-        type="password"
-        id="password"
-        name="password"
-        value={form?.data?.password ?? ''}
-        error={isErrorStatus(form?.status) && form?.field === 'password' ? form.message : ''}
-        appearance="secondary-primary"
-      />
+      <div class="input-field-container {isErrorStatus(form?.status) && !form?.field ? 'with-form-error' : ''}">
+        <InputField
+          label="Password"
+          type="password"
+          id="password"
+          name="password"
+          value={form?.data?.password ?? ''}
+          error={isErrorStatus(form?.status) && form?.field === 'password' ? form.message : ''}
+          appearance="secondary-primary"
+        />
+      </div>
   
+      {#if isErrorStatus(form?.status) && !form?.field}
+        <div class="error-container">
+          <ErrorText>
+            {form?.message}
+          </ErrorText>
+        </div>
+      {/if}
+
       <Button>Sign In</Button>
       <a href="/signup">Don't have an account?</a>
     </form>
@@ -57,9 +73,23 @@
     max-width: 400px;
     margin: 0 auto;
 
+    .input-field-container {
+      --input-field-margin-bottom: 0;
+
+      margin-bottom: 1rem;
+
+      &.with-form-error {
+        margin-bottom: 0;
+      }
+    }
+
     a {
       margin-top: 1rem;
       text-align: center;
     }
+  }
+
+  .error-container {
+    margin-bottom: 1rem;
   }
 </style>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { enhance } from "$app/forms";
-	import Button from "$lib/components/Button.svelte";
+  import { enhance } from "$app/forms";
+  import Button from "$lib/components/Button.svelte";
+  import ErrorText from "$lib/components/ErrorText.svelte";
   import InputField from "$lib/components/InputField.svelte";
-	import Page from "$lib/components/Page.svelte";
+  import Page from "$lib/components/Page.svelte";
   import { isErrorStatus } from "$lib/helpers/response";
-	import type { ActionData } from "./$types";
+  import type { ActionData } from "./$types";
   
   export let form: ActionData;
 </script>
@@ -13,35 +14,49 @@
   <div class="signup-container">
     <h1>Sign up</h1>
     <form method="POST" use:enhance>
-      <InputField
-        label="Email"
-        type="email"
-        id="email"
-        name="email"
-        value={form?.data?.email ?? ''}
-        error={isErrorStatus(form?.status) && form?.field === 'email' ? form.message : ''}
-        appearance="secondary-primary"
-      />
+      <div class="input-field-container">
+        <InputField
+          label="Email"
+          type="email"
+          id="email"
+          name="email"
+          value={form?.data?.email ?? ''}
+          error={isErrorStatus(form?.status) && form?.field === 'email' ? form.message : ''}
+          appearance="secondary-primary"
+        />
+      </div>
   
-      <InputField
-        label="Password"
-        type="password"
-        id="password"
-        name="password"
-        value={form?.data?.password ?? ''}
-        error={isErrorStatus(form?.status) && form?.field === 'password' ? form.message : ''}
-        appearance="secondary-primary"
-      />
+      <div class="input-field-container">
+        <InputField
+          label="Password"
+          type="password"
+          id="password"
+          name="password"
+          value={form?.data?.password ?? ''}
+          error={isErrorStatus(form?.status) && form?.field === 'password' ? form.message : ''}
+          appearance="secondary-primary"
+        />
+      </div>
   
-      <InputField
-        label="Confirm Password"
-        type="password"
-        id="confirmedPassword"
-        name="confirmedPassword"
-        value={form?.data?.confirmedPassword ?? ''}
-        error={isErrorStatus(form?.status) && form?.field === 'confirmedPassword' ? form.message : ''}
-        appearance="secondary-primary"
-      />
+      <div class="input-field-container {isErrorStatus(form?.status) && !form?.field ? 'with-form-error' : ''}">
+        <InputField
+          label="Confirm Password"
+          type="password"
+          id="confirmedPassword"
+          name="confirmedPassword"
+          value={form?.data?.confirmedPassword ?? ''}
+          error={isErrorStatus(form?.status) && form?.field === 'confirmedPassword' ? form.message : ''}
+          appearance="secondary-primary"
+        />
+      </div>
+
+      {#if isErrorStatus(form?.status) && !form?.field}
+        <div class="error-container">
+          <ErrorText>
+            {form?.message}
+          </ErrorText>
+        </div>
+      {/if}
   
       <Button>Sign Up</Button>
       <a href="/signin">Already have an account?</a>
@@ -67,9 +82,23 @@
     max-width: 400px;
     margin: 0 auto;
 
+    .input-field-container {
+      --input-field-margin-bottom: 0;
+
+      margin-bottom: 1rem;
+
+      &.with-form-error {
+        margin-bottom: 0;
+      }
+    }
+
     a {
       margin-top: 1rem;
       text-align: center;
     }
+  }
+
+  .error-container {
+    margin-bottom: 1rem;
   }
 </style>
